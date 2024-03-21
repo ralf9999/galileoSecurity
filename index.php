80c08e3d6413cd011b11ec8fab524270a0610390
ralf9999 <info@heinprojects.de>
2024-03-21 12:01:24 +0100

    neues Update index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ Template Name: Boxing Seite
+ Template Post Type: page
+*/
+?>
+<?php get_header('homemit'); ?>
+<main id="id_main_area" class="cl_boxingArea">
+    <?php the_content()?>
+</main>
+<?php get_footer(); ?>
